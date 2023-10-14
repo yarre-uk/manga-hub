25ed5f66e61dc7c88f00131b810208418b8ee9ee
Yarre <yaroslav.syvukha@gmail.com>
2023-10-14 15:33:34 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { AppStyled, HelloWorldStyled } from './AppStyles';
+
+function App() {
+  return (
+    <AppStyled>
+      <HelloWorldStyled>Hello World</HelloWorldStyled>
+      <HelloWorldStyled>Hello World</HelloWorldStyled>
+    </AppStyled>
+  );
+}
+
+export default App;
---
+++ src/AppStyles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const AppStyled = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const HelloWorldStyled = styled.p`
+  font-size: 2rem;
+`;
