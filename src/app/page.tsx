34a1c53e2ce3b887ea8eb1dea2d4942d237b5e69
Yarre <yaroslav.syvukha@gmail.com>
2023-10-26 17:55:07 +0200

    feat: add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Input } from '@/shared/components/ui/input';
+
 export default function Page() {
-  return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>;
+  return (
+    <div>
+      <Input type="email" placeholder="Email" />
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+    </div>
+  );
 }
