e5ab529ccb9a95f1ddc51e66220c4802411f86a6
Yarre <yaroslav.syvukha@gmail.com>
2023-10-26 17:05:59 +0200

    feat: add tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
 export default function Page() {
-  return (
-    <div>
-      <p className="text-3xl font-bold underline">asd</p>
-      {/* <Button variant="outline">Button</Button> */}
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>;
 }
