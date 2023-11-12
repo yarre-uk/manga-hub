b5bbed8667211111c7b5a0a97693f9642273ab9c
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2023-11-12 22:09:22 +0200

    feat: sidebar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,13 @@
 import '../globals.css';
 
+import dynamic from 'next/dynamic';
 import { ReactNode } from 'react';
 
 import Header from '@/shared/components/Header';
 import Providers from '@/shared/components/Providers';
+const Sidebar = dynamic(() => import('@/shared/components/Sidebar/Sidebar'), {
+  ssr: false,
+});
 import { Toaster } from '@/shared/components/ui/toaster';
 
 interface RootLayoutProps {
@@ -17,6 +21,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <Providers>
           <Header />
           <Toaster />
+          <Sidebar />
           <div className="h-[calc(100vh-56px)]">{children}</div>
         </Providers>
       </body>
