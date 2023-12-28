d8957ea5344039b614c3ac9d391d5b8938be5d82
Yarre <yaroslav.syvukha@gmail.com>
2023-12-28 19:30:38 +0100

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { ROUTE } from '@/shared/constants/routes';
+
+const App = () => {
   return (
-    <div>
-      <p>App</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path={ROUTE.HOME} element={<LayoutContainer />}> */}
+        <Route index element={<div>Home</div>} />
+        <Route path={ROUTE.SIGN_IN} element={<div>SignIn</div>} />
+        <Route path={ROUTE.SIGN_UP} element={<div>SignUp</div>} />
+        <Route path="/*" element={<div>Page404</div>} />
+        {/* </Route> */}
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
