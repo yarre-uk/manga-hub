cb84e9ef40c590fffd6190dd4141cea03769d5aa
Yarre <yaroslav.syvukha@gmail.com>
2023-12-28 19:45:51 +0100

    refactor: moving files and folders from src/shared to src/

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,13 +1,11 @@
 import { default as _axios } from 'axios';
 
-import { API_URL } from '../constants/common';
-
 export const axios = _axios.create({
-  baseURL: API_URL,
+  // baseURL: API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
 export const axiosAuth = _axios.create({
-  baseURL: API_URL,
+  // baseURL: API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
