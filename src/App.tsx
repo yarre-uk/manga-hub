b8f2dcb791bf10e2a22e7d195b8fe9a98c939018
Yarre <yaroslav.syvukha@gmail.com>
2023-12-29 12:33:50 +0100

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { ROUTE } from '@/shared/constants/routes';
+import { ROUTE } from '@/constants/routes';
+import GlobalStyles from '@/globals';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path={ROUTE.HOME} element={<LayoutContainer />}> */}
-        <Route index element={<div>Home</div>} />
-        <Route path={ROUTE.SIGN_IN} element={<div>SignIn</div>} />
-        <Route path={ROUTE.SIGN_UP} element={<div>SignUp</div>} />
-        <Route path="/*" element={<div>Page404</div>} />
-        {/* </Route> */}
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path={ROUTE.HOME} element={<LayoutContainer />}> */}
+          <Route index element={<div>Home</div>} />
+          <Route path={ROUTE.SIGN_IN} element={<div>SignIn</div>} />
+          <Route path={ROUTE.SIGN_UP} element={<div>SignUp</div>} />
+          <Route path="/*" element={<div>Page404</div>} />
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
