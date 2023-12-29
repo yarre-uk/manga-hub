90fd4abdecbda3da8defe197df13c4bbe4360455
Yarre <yaroslav.syvukha@gmail.com>
2023-12-29 22:22:49 +0100

    feat: deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,45 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
-import { ROUTE } from './constants';
+import { LayoutContainer } from './modules/layout';
+
+import { ROUTE } from '@/constants';
+import GlobalStyles from '@/globals';
 import {
   ChangePasswordContainer,
   ForgotPasswordContainer,
   SignInContainer,
   SignUpContainer,
-} from './modules/auth';
-import { HomeContainer } from './modules/home';
-import { LayoutContainer } from './modules/layout';
-import { NotFoundContainer } from './modules/notFound';
-
-import GlobalStyles from '@/globals';
+} from '@/modules/auth';
+import { HomeContainer } from '@/modules/home';
+import { NotFoundContainer } from '@/modules/notFound';
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate(ROUTE.HOME);
+  }, []);
+
   return (
     <>
       <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route path={ROUTE.HOME} element={<LayoutContainer />}>
-            <Route index element={<HomeContainer />} />
-            <Route path={ROUTE.SIGN_IN} element={<SignInContainer />} />
-            <Route path={ROUTE.SIGN_UP} element={<SignUpContainer />} />
-            <Route
-              path={ROUTE.FORGOT_PASSWORD}
-              element={<ForgotPasswordContainer />}
-            />
-            <Route
-              path={ROUTE.CHANGE_PASSWORD}
-              element={<ChangePasswordContainer />}
-            />
-            <Route path="/*" element={<NotFoundContainer />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path={ROUTE.HOME} element={<LayoutContainer />}>
+          <Route index element={<HomeContainer />} />
+          <Route path={ROUTE.SIGN_IN} element={<SignInContainer />} />
+          <Route path={ROUTE.SIGN_UP} element={<SignUpContainer />} />
+          <Route
+            path={ROUTE.FORGOT_PASSWORD}
+            element={<ForgotPasswordContainer />}
+          />
+          <Route
+            path={ROUTE.CHANGE_PASSWORD}
+            element={<ChangePasswordContainer />}
+          />
+          <Route path="/*" element={<NotFoundContainer />} />
+        </Route>
+      </Routes>
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />,
+  <HashRouter>
+    <App />
+  </HashRouter>,
 );
